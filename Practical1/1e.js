adc6c216a73763e734b545007d5ecc7daed7e7db
vinapadhmvlu-hue <vinapadhmvlu@gmail.com>
2026-07-18 23:23:42 +0530

    Add basic comparison and switch case example

---
+++ Practical1/1e.js
@@ -0,0 +1,24 @@
+let num = 15; 
+let num2 = 10; 
+if(num>num2) 
+{ 
+ console.log("Number 1 is greater",num); 
+} 
+else{ 
+ console.log("Number 2 is greater",num2) 
+} 
+let day = 2; 
+switch(day){ 
+ case 1: 
+ console.log("Monday"); 
+ break; 
+ case 2: 
+ console.log("Teusday"); 
+ break; 
+ case 3: 
+ console.log("Wednesday"); 
+ break; 
+ default: 
+ console.log("Invalid day") 
+} 
+console.log("Vinayak padhy T029") 
