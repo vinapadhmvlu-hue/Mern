16ce0a1ea824dfd5f76996721ed5afeef3cc7208
vinapadhmvlu-hue <vinapadhmvlu@gmail.com>
2026-07-18 23:22:25 +0530

    Add basic arithmetic operations in 1c.js

---
+++ Practical1/1c.js
@@ -0,0 +1,9 @@
+let num = 15; 
+let num2 = 10;
+console.log("First number = ", num); 
+console.log("Second number = ", num2); 
+console.log("Addition = ", num+num2); 
+console.log("Subtraction = ", num-num2); 
+console.log("Mutiplication = ", num*num2); 
+console.log("Division = ", num/num2); 
+console.log("Vinayak padhy T029") 
