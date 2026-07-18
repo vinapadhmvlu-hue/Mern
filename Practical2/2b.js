4ca1ffa515bb7b708f6293ed8da102a050de7a3c
vinapadhmvlu-hue <vinapadhmvlu@gmail.com>
2026-07-18 12:20:02 +0530

    Create 2b.js

---
+++ Practical2/2b.js
@@ -0,0 +1,9 @@
+let fruits = ["Mango","Banana","Apple"]; 
+let student = { 
+ name: "vinayak", 
+ age: 19, 
+ city: "Mumbai" 
+}; 
+console.log("Fruits :",fruits); 
+console.log("Second Fruits :",fruits[1]); 
+console.log("Student Name:", student.name); console.log("Student Age: ",student.age); console.log("Student City:", student.city); 
