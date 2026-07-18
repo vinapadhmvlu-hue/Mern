a91ba670e89531fb9e2f8a74dbc37f38360fd648
vinapadhmvlu-hue <vinapadhmvlu@gmail.com>
2026-07-18 23:23:05 +0530

    Add basic arithmetic functions and logging

---
+++ Practical1/1d.js
@@ -0,0 +1,12 @@
+let num = 15; 
+let num2 = 10; 
+function Add(a,b) 
+{ 
+ return a+b; 
+} 
+function Sub(a,b) 
+{ 
+ return a-b; 
+} 
+console.log("Addition = ",Add(num,num2)); 
+console.log("Subtraction = ",Sub(num,num2)); console.log("Vinayak padhy T029") 
