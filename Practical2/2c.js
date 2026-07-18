25aca8066b2ec6fe78df0a4423ba763b890565d8
vinapadhmvlu-hue <vinapadhmvlu@gmail.com>
2026-07-18 12:20:41 +0530

    Create 2c.js

---
+++ Practical2/2c.js
@@ -0,0 +1,13 @@
+let add = (a,b) => { 
+ return a+b; 
+}; 
+let sub = (a,b) => { 
+ return a-b; 
+}; 
+let mul = (a,b) => { 
+ return a*b; 
+}; 
+console.log("Sum = ",add(10,20)); 
+console.log("Sub = ",add(10,20)); 
+console.log("Mul = ",mul(5,9)); 
+console.log("vinayak"); 
