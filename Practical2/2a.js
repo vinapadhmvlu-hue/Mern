14d9763cbd0d1aa5202c35619e86d446650f376c
vinapadhmvlu-hue <vinapadhmvlu@gmail.com>
2026-07-18 12:19:14 +0530

    Create 2a.js

---
+++ Practical2/2a.js
@@ -0,0 +1,10 @@
+function calculateSalary(name, basicSalary) 
+{ 
+let bonus = 5000; 
+let totalSalary = basicSalary + bonus; 
+console.log("Employee Name = ", name); 
+console.log("Bsaic Salary = ", basicSalary); 
+console.log("Bonus = ", bonus); 
+console.log("Total salary = ", totalSalary); 
+} 
+calculateSalary("vinayak padhy", 50000); 
