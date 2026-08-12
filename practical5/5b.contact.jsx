b5674744e968ecc09cd966fca4b28f9fe74855cd
vinapadhmvlu-hue <vinapadhmvlu@gmail.com>
2026-08-12 13:45:03 +0530

    Add Contact component with email information

---
+++ practical5/5b.contact.jsx
@@ -0,0 +1,9 @@
+function Contact(){
+    return (
+        <div>
+            <h1>Contact Page</h1>
+            <p>Email: vinapadhmvlu@gmail.com</p>
+        </div>
+    );
+}
+export default Contact;
